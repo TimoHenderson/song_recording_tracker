a1b43c17c14bad56b54548db08cef3a4e641b7b3
TimoHenderson <tjhenderson2@gmail.com>
2023-02-17 21:17:09 +0000

    Added run_sql.py

---
+++ recording_tracker/db/run_sql.py
@@ -0,0 +1,21 @@
+import psycopg2
+import psycopg2.extras as ext
+
+
+def run_sql(sql, values=None):
+    conn = None
+    results = []
+
+    try:
+        conn = psycopg2.connect("dbname='recording_tracker'")
+        cur = conn.cursor(cursor_factory=ext.DictCursor)
+        cur.execute(sql, values)
+        conn.commit()
+        results = cur.fetchall()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+    return results
