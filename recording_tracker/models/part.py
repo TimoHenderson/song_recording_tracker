abfa5be71d15e1871ff13317c845942331dd4631
TimoHenderson <tjhenderson2@gmail.com>
2023-02-17 15:52:02 +0000

    added part.status

--- recording_tracker/models/part.py
+++ recording_tracker/models/part.py
@@ -1,3 +1,8 @@
 class Part:
-    def __init__(self, name):
+    def __init__(
+        self,
+        name,
+        status,
+    ):
         self.name = name
+        self.status = status
--- recording_tracker/tests/part_test.py
+++ recording_tracker/tests/part_test.py
@@ -4,9 +4,14 @@ from models.part import Part
 
 class TestPart(unittest.TestCase):
     def setUp(self):
-        self.guitar1 = Part("Verse Chords")
+        self.guitar1 = Part("Verse Chords", 5)
 
     def test_has_name(self):
         actual = self.guitar1.name
         expected = "Verse Chords"
+        self.assertEqual(actual, expected)
+
+    def test_has_status(self):
+        actual = self.guitar1.status
+        expected = 5
         self.assertEqual(actual, expected)
