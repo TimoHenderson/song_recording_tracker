f61eddc014d5f737016a392705e4409a31230f25
TimoHenderson <tjhenderson2@gmail.com>
2023-02-19 16:39:58 +0000

    Updated db schema to add instruments table

--- recording_tracker/db/recording_tracker.sql
+++ recording_tracker/db/recording_tracker.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS parts;
+DROP TABLE IF EXISTS instruments;
 DROP TABLE IF EXISTS songs;
 
 CREATE TABLE songs(
@@ -9,11 +10,18 @@ CREATE TABLE songs(
     notes TEXT
 );
 
+CREATE TABLE instruments(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    icon VARCHAR(255)
+);
+
 CREATE TABLE parts(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     status INT,
     song_id INT REFERENCES songs(id) ON DELETE CASCADE,
-    instrument VARCHAR(255),
+    instrument_id INT REFERENCES instruments(id) ON DELETE CASCADE ,
     notes TEXT
 );
+
