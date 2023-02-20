665dc9ed4dff612a839da6f4ef1a2fa5dcd7fa66
TimoHenderson <tjhenderson2@gmail.com>
2023-02-20 16:50:51 +0000

    Updated seeds.py

--- recording_tracker/seeds.py
+++ recording_tracker/seeds.py
@@ -6,93 +6,20 @@ from models.part import Part
 from models.instrument import Instrument
 
 
-def print_songs():
-    songs = song_repository.select_all()
-    if songs:
-        [print(song.__dict__) for song in songs]
-    else:
-        print("No songs")
+instrument1 = Instrument("Guitar", "e900")
+instrument_repository.save(instrument1)
+
+instrument2 = Instrument("Drums", "e92c")
+instrument_repository.save(instrument2)
 
 
-def print_parts():
-    parts = part_repository.select_all()
-    if parts:
-        [print(part.__dict__) for part in parts]
-    else:
-        print("No parts")
+song1 = Song("SDKMN", "The Purple Felts", "The Jester's Game", "Total Rubbish")
+song_repository.save(song1)
 
+song2 = Song("The Jester", "The Purple Felts", "The Jester's Game", "Really Good")
+song_repository.save(song1)
 
-def print_instruments():
-    instruments = instrument_repository.select_all()
-    if instruments:
-        [print(instrument.__dict__) for instrument in instruments]
-    else:
-        print("No instruments")
-
-
-guitar = Instrument("Guitar", "fa-guitar")
-instrument_repository.save(guitar)
-
-drum = Instrument("Drums", "fa-drum")
-instrument_repository.save(drum)
-
-print_instruments()
-# song3 = Song("SDKMN", "The Purple Felts", "The Jester's Game")
-# song_repository.save(song3)
-
-# song1 = Song(
-#     "The Jester",
-#     "The Purple Felts",
-#     "The Jester's Game",
-#     "This needs more parts",
-# )
-# song_repository.save(song1)
-
-# print_songs()
-
-
-breakpoint()
-
-# song3.title = "XBGER"
-# song_repository.update(song3)
-# print_songs()
-
-# song = song_repository.select(1)
-# print(song.__dict__)
-
-# song_repository.delete_all()
-# print_songs()
-
-# song_repository.delete(9)
-# print_songs()
-
-# guitar1 = Part("guitar1", 5, 1)
-# part_repository.save(guitar1)
-# guitar2 = Part("Guitar 2", 5, 1)
-# part_repository.save(guitar2)
-# guitar3 = Part("Guitar 3", 5, 2)
-# part_repository.save(guitar3)
-
-# got_guitar = song_repository.select(1)
-
-# [print(part.__dict__) for part in part_repository.select_all_with_song(got_guitar.id)]
-
-# print_parts()
-
-# print(part_repository.select(2).__dict__)
-
-# guitar1.name = "Egg"
-# part_repository.update(guitar1)
-
-
-# print()
-# print_parts()
-
-# part_repository.delete(3)
-
-# print()
-# print_parts()
-
-# part_repository.delete_all()
-# print()
-# print_parts()
+part1 = Part("Verse Chords", 3, song1.id, instrument1, "Needs to be better")
+part_repository.save(part1)
+part2 = Part("End Shred", 3, song1.id, instrument2, "Needs to be better")
+part_repository.save(part2)
