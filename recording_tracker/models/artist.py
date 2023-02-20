74da37d32aedb56291cc28ed86dd6260572b1c0f
TimoHenderson <tjhenderson2@gmail.com>
2023-02-20 19:09:31 +0000

    Added Artist.id

--- recording_tracker/models/artist.py
+++ recording_tracker/models/artist.py
@@ -4,3 +4,4 @@ from dataclasses import dataclass
 @dataclass
 class Artist:
     name: str
+    id: int = None
--- recording_tracker/tests/artist_test.py
+++ recording_tracker/tests/artist_test.py
@@ -5,8 +5,19 @@ from models.artist import Artist
 class TestArtist(unittest.TestCase):
     def setUp(self):
         self.artist1 = Artist("The Purple Felts")
+        self.artist2 = Artist("U2", 1)
 
     def test_has_name(self):
         actual = self.artist1.name
         expected = "The Purple Felts"
+        self.assertEqual(actual, expected)
+
+    def test_has_id__no_id(self):
+        actual = self.artist1.id
+        expected = None
+        self.assertEqual(actual, expected)
+
+    def test_has_id__has_id(self):
+        actual = self.artist2.id
+        expected = 1
         self.assertEqual(actual, expected)
