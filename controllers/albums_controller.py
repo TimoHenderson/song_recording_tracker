4d20bd2f53736c09cb93830e3a8eb2c2bedb17f3
TimoHenderson <tjhenderson2@gmail.com>
2023-02-21 09:14:11 +0000

    added album_controller new and create

--- controllers/albums_controller.py
+++ controllers/albums_controller.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect
 from repositories import album_repository
+from repositories import artist_repository
 from models.album import Album
 
 
@@ -17,3 +18,20 @@ def show_all():
 def show(id):
     album = album_repository.select(id)
     return render_template("albums/show.html", album=album)
+
+
+# New
+@albums_blueprint.route("/albums/new")
+def new_album():
+    artists = artist_repository.select_all()
+    return render_template("albums/new.html", artists=artists)
+
+
+# Create
+@albums_blueprint.route("/albums", methods=["POST"])
+def create():
+    form = request.form
+    artist = artist_repository.select(form["artist_id"])
+    new_album = Album(form["name"], artist)
+    album_repository.save(new_album)
+    return redirect("/albums")
