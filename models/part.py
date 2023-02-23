24b5b4fcd6f16350a22308d6517998dbfba6c3cb
TimoHenderson <tjhenderson2@gmail.com>
2023-02-23 00:49:14 +0000

    changed part.get_status_str to if-elif

--- models/part.py
+++ models/part.py
@@ -13,46 +13,16 @@ class Part:
     id: int = None
 
     def get_status_str(self):
-        match self.status:
-            case 0:
-                status_str = "Not Started"
-            case 1:
-                status_str = "Tracking Guides"
-            case 2:
-                status_str = "Tracking Part"
-            case 3:
-                status_str = "Doing drop ins"
-            case 4:
-                status_str = "Comping"
-            case 5:
-                status_str = "Take!"
+        if self.status == 0:
+            status_str = "Not Started"
+        elif self.status == 1:
+            status_str = "Tracking Guides"
+        elif self.status == 2:
+            status_str = "Tracking Part"
+        elif self.status == 3:
+            status_str = "Doing drop ins"
+        elif self.status == 4:
+            status_str = "Comping"
+        elif self.status == 5:
+            status_str = "Take!"
         return status_str
-
-
-# class Part:
-#     def __init__(self, name, status, song_id, instrument=None, notes="", id=None):
-#         self.name = name
-#         self.status = status
-#         self.song_id = song_id
-#         self.instrument = instrument
-#         self.notes = notes
-#         self.id = id
-
-#     def get_status_str(self):
-#         match self.status:
-#             case 0:
-#                 status_str = "Not Started"
-#             case 1:
-#                 status_str = "Tracking Guides"
-#             case 2:
-#                 status_str = "Tracking Part"
-#             case 3:
-#                 status_str = "Doing drop ins"
-#             case 4:
-#                 status_str = "Comping"
-#             case 5:
-#                 status_str = "Take!"
-#         return status_str
-
-#     def __repr__(self):
-#         return f"{self.name}, {self.status}"
--- models/song.py
+++ models/song.py
@@ -25,27 +25,3 @@ class Song:
 
     def get_parts_status_percent(self):
         return [status * 20 for status in self.parts_status]
-
-
-# class Song:
-#     def __init__(self, title, artist, album, notes="", parts=[], id=None):
-#         self.title = title
-#         self.artist = artist
-#         self.album = album
-#         self.notes = notes
-#         self.parts_status = parts
-#         self.id = id
-
-#     def get_completion(self):
-#         completion = 0
-#         if self.parts_status:
-#             total = sum(status for status in self.parts_status)
-#             possible = len(self.parts_status) * 5
-#             completion = total / possible * 100
-#         return int(completion)
-
-#     def get_num_parts(self):
-#         return len(self.parts_status)
-
-#     def __repr__(self):
-#         return f"{self.title}, {self.artist}"
