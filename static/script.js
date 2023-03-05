ece6dcbb3bd4003f46af1bce1b018657817a6149
TimoHenderson <tjhenderson2@gmail.com>
2023-03-05 23:09:04 +0000

    Changed card menu  to use js

--- static/script.js
+++ static/script.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuButton = document.getElementById("menu-button");
     menuButton.addEventListener("click", handleMenuButton);
+
+    const cardMenuButtons = document.querySelectorAll(".card-menu-button")
+    cardMenuButtons.forEach(cardMenuButton => cardMenuButton.addEventListener("click", handleCardMenuButton));
 })
 
 function handleMenuButton() {
@@ -12,5 +15,16 @@ function handleMenuButton() {
     } else {
         navPane.className = "";
         links.forEach(link => link.tabIndex = -1);
+    }
+}
+
+function handleCardMenuButton(event) {
+    const cardMenu = event.target.parentNode.parentNode.nextElementSibling;
+    if (cardMenu.classList == "card-menu") {
+        cardMenu.classList = "card-menu show";
+        event.target.classList = "fa-solid fa-ellipsis fa-2x close-card-menu"
+    } else {
+        cardMenu.classList = "card-menu";
+        event.target.classList = "fa-solid fa-ellipsis-vertical fa-2x open-card-menu"
     }
 }
